022b96697a7e23688d3649f5b457d42eb9c1c933
maddyBoy <inrsaurabh@gmail.com>
2018-01-25 23:48:11 +0530

    initail check of fork

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'thedesideveloper');
+define('DB_NAME', 'thedesidvelopers');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'admin');
+define('DB_PASSWORD', 'hestabit');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
